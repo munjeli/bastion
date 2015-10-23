5dbf28a502c862faafc180fb24d379d0b38a49d3
Jonathan Hartman <j@p4nt5.com>
2015-10-23 15:15:10 -0700

    Implement a test wrapper to ensure rsyslog is running
    One of the integration tests verifies that `/var/log/auth.log` is
    getting the expected Snoopy logs, but the containers used in CI by
    `kitchen-docker` don't come with rsyslog running amd don't have a
    working `/sbin/start` so the default `service` resource won't work.

--- Berksfile
+++ Berksfile
@@ -3,3 +3,5 @@
 source 'https://supermarket.chef.io'
 
 metadata
+
+cookbook 'bastion_test', path: 'test/fixtures/cookbooks/bastion_test'
---
+++ test/fixtures/cookbooks/bastion_test/metadata.rb
@@ -0,0 +1,15 @@
+# Encoding: UTF-8
+#
+# rubocop:disable SingleSpaceBeforeFirstArg
+name             'bastion_test'
+maintainer       'Jonathan Hartman'
+maintainer_email 'jonathan.hartman@socrata.com'
+license          'apache2'
+description      'bastion_test'
+long_description 'bastion_test'
+version          '0.0.1'
+
+depends          'bastion'
+
+supports         'ubuntu'
+# rubocop:enable SingleSpaceBeforeFirstArg
